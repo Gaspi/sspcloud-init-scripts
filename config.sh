986a0b4c4360998edcdfd69b16dfc2df54f6d77b
Gaspard Férey <gaspard.ferey@gmail.com>
2025-01-14 12:02:00 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -44,3 +44,5 @@ git clone https://github.com/InseeFrLab/helm-charts-shiny-apps.git
 git clone https://github.com/InseeFrLab/helm-charts-miscellaneous.git
 git clone https://inseefrlab.github.io/helm-charts-datascience
 
+ls -d **/charts/* | xargs -i{} bash -c "cd charts/{} && helm dep update"
+
